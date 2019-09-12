3d83be1d37fe4ae10f88a484c6c6f9ca30acdaf8
Pectoraux <ekontetevi@gmail.com>
2019-09-12 13:13:18 +0000

    First commit

---
+++ producers/models/turnstile.py
@@ -0,0 +1,74 @@
+"""Creates a turnstile data producer"""
+import logging
+from pathlib import Path
+
+from confluent_kafka import avro
+
+from models.producer import Producer
+from models.turnstile_hardware import TurnstileHardware
+
+
+logger = logging.getLogger(__name__)
+TURNSTILE_TOPIC = "TURNSTILE"
+
+class Turnstile(Producer):
+    key_schema = avro.load(f"{Path(__file__).parents[0]}/schemas/turnstile_key.json")
+
+    #
+    # TODO: Define this value schema in `schemas/turnstile_value.json, then uncomment the below
+    #
+    value_schema = avro.load(
+f"{Path(__file__).parents[0]}/schemas/turnstile_value.json")
+
+    def __init__(self, station):
+        """Create the Turnstile"""
+        station_name = (
+            station.name.lower()
+            .replace("/", "_and_")
+            .replace(" ", "_")
+            .replace("-", "_")
+            .replace("'", "")
+        )
+
+        #
+        #
+        # TODO: Complete the below by deciding on a topic name, number of partitions, and number of
+        # replicas
+        #
+        #
+        super().__init__(
+            f"{TURNSTILE_TOPIC}", # TODO: Come up with a better topic name
+            key_schema=Turnstile.key_schema,
+            # TODO: 
+            value_schema=Turnstile.value_schema, 
+#             TODO: Uncomment once schema is defined
+            TODO: num_partitions=2,
+            TODO: num_replicas=1,
+        )
+        self.station = station
+        self.turnstile_hardware = TurnstileHardware(station)
+
+    def run(self, timestamp, time_step):
+        """Simulates riders entering through the turnstile."""
+        num_entries = self.turnstile_hardware.get_entries(timestamp, time_step)
+        logger.info("turnstile kafka integration incomplete - skipping")
+        #
+        #
+        # TODO: Complete this function by emitting a message to the turnstile topic for the number
+        # of entries that were calculated
+        #
+        #
+        self.producer.produce(
+           topic=TURNSTILE_TOPIC,
+           key={"timestamp": self.time_millis()},
+           value={
+               #
+               #
+               # TODO: Configure this
+               "station_id": self.station_id, 
+               "station_name": station_name,
+               "line": self.station.color
+               #
+               #
+           },
+        )
---
+++ startup.sh
@@ -0,0 +1,42 @@
+echo 'schema.registry.url=http://localhost:8081' >> /etc/kafka/connect-distributed.properties
+systemctl start confluent-zookeeper
+systemctl start confluent-kafka
+systemctl start confluent-schema-registry
+systemctl start confluent-kafka-rest
+systemctl start confluent-kafka-connect
+systemctl start confluent-ksql
+sed -i 's/md5/trust/g' /etc/postgresql/10/main/pg_hba.conf
+/etc/init.d/postgresql start
+su postgres -c "createuser root -s"
+createdb classroom
+psql -d classroom -c "DROP TABLE IF EXISTS purchases; CREATE TABLE purchases(id INT PRIMARY KEY, username VARCHAR(100), currency VARCHAR(10), amount INT);"
+psql -d classroom -c "DROP TABLE IF EXISTS clicks; CREATE TABLE clicks(id INT PRIMARY KEY, email VARCHAR(100), timestamp VARCHAR(100), uri VARCHAR(512), number INT);"
+psql -d classroom -c "DROP TABLE IF EXISTS connect_purchases; CREATE TABLE connect_purchases(id INT PRIMARY KEY, username VARCHAR(100), currency VARCHAR(10), amount INT);"
+psql -d classroom -c "DROP TABLE IF EXISTS connect_clicks; CREATE TABLE connect_clicks(id INT PRIMARY KEY, email VARCHAR(100), timestamp VARCHAR(100), uri VARCHAR(512), number INT);"
+psql -d classroom -c "COPY purchases(id,username,currency,amount)  FROM '/home/workspace/purchases.csv' DELIMITER ',' CSV HEADER;"
+psql -d classroom -c "COPY clicks(id,email,timestamp,uri,number)  FROM '/home/workspace/clicks.csv' DELIMITER ',' CSV HEADER;"
+
+su postgres -c "createuser cta_admin -s"
+createdb cta
+psql -d cta -c "ALTER USER cta_admin WITH PASSWORD 'chicago'"
+psql -d cta -c "CREATE TABLE stations (stop_id INTEGER PRIMARY KEY, direction_id VARCHAR(1) NOT NULL, stop_name VARCHAR(70) NOT NULL, station_name VARCHAR(70) NOT NULL, station_descriptive_name VARCHAR(200) NOT NULL, station_id INTEGER NOT NULL, \"order\" INTEGER, red BOOLEAN NOT NULL, blue BOOLEAN NOT NULL, green BOOLEAN NOT NULL);"
+
+psql -d cta -c "COPY stations(stop_id, direction_id,stop_name,station_name,station_descriptive_name,station_id,\"order\",red,blue,green) FROM '/home/workspace/cta_stations.csv' DELIMITER ',' CSV HEADER;"
+
+# Configure lesson 6 and 7 streams
+kafka-topics --delete --zookeeper localhost:2181 --topic com.udacity.streams.users
+kafka-topics --delete --zookeeper localhost:2181 --topic com.udacity.streams.purchases
+kafka-topics --create --zookeeper localhost:2181 --topic com.udacity.streams.users --replication-factor 1 --partitions 10
+kafka-topics --create --zookeeper localhost:2181 --topic com.udacity.streams.purchases --replication-factor 1 --partitions 10
+kafka-topics --delete --zookeeper localhost:2181 --topic com.udacity.streams.pages
+kafka-topics --delete --zookeeper localhost:2181 --topic com.udacity.streams.clickevents
+kafka-topics --create --zookeeper localhost:2181 --topic com.udacity.streams.pages --replication-factor 1 --partitions 10
+kafka-topics --create --zookeeper localhost:2181 --topic com.udacity.streams.clickevents --replication-factor 1 --partitions 10
+
+# Configure the directory structure for KSQL
+mkdir -p /var/lib/kafka-streams
+chmod g+rwx /var/lib/kafka-streams
+chgrp -R confluent /var/lib/kafka-streams
+
+(python stream.py &) &
+(python clicks.py &) &
